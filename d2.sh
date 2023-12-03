251203110cf9b0659089f85ed39ddf8434e5ad0d
Tor Turner <tor.turner@matillion.com>
2023-12-03 04:32:17 +0000

    tuning day 2

--- d2.sh
+++ d2.sh
@@ -1,4 +1,4 @@
-#! /usr/bin/env bash
+#!/usr/bin/env bash
 
 # Warning: the following solution is messy as heck
 
@@ -9,65 +9,61 @@ power_array=()
 #touch newfile.txt
 #: >newfile.txt
 
+case $operation in
+	1)
+		echo "Solving puzzle 1"
+	;;
+	2)
+		echo "Solving puzzle 2"
+	;;
+	*)
+		echo "Invalid arguments. Usage: ./d2sh filename (1|2)"
+		exit 1
+	;;
+esac
+
+declare -A limits=(["blue"]=14 ["green"]=13 ["red"]=12)
+
 while read line; do
 	game_id=$(echo $line | awk '{ print $2 }' | cut -f1 -d\:)
 	amended_line=$(awk -F ':' '{ print $2 }' <<< $line )
 	IFS=';' read -ra games <<< "${amended_line}"
+	declare -A maximums=(["blue"]=0 ["green"]=0 ["red"]=0)
+	pass="true"
+	for i in "${games[@]}"; do
+		IFS=',' read -ra stages <<< "${i}"
+		for stage in "${stages[@]}"; do
+			n=$(awk '{ print $1 }' <<< $stage)
+			number=0
+			let number+=$n
+			colour=$(awk '{ print $2 }' <<< $stage)
+			l=${limits["${colour}"]}
+			limit=0
+			let limit+=$l
+			m=${maximums["${colour}"]}
+			max=0
+			let max+=$m
+			if [[ $operation -eq 2 ]]; then
+				if [[ $number -gt $max ]]; then
+					maximums["${colour}"]=$number
+				fi
+			else
+				if [[ $number -gt $limit ]]; then
+					pass="false"
+				fi
+			fi
+		done
+	done
 	if [[ $operation -eq 2 ]]; then
 		blue=0
 		red=0
 		green=0
-	elif [[ $operation -eq 1 ]]; then
-		pass="true"
-	else
-		echo "Invalid argument - must provide argument as to which puzzle you are trying to solve. Usage: ./d2sh filename (1|2)"
-		exit 1
-	fi
-	for i in "${games[@]}"; do
-		IFS=',' read -ra stages <<< "${i}"
-		for stage in "${stages[@]}"; do	
-			n=$(awk '{ print $1 }' <<< $stage)
-			number=0
-			let number+=$n
-			colour=$(awk '{ print $2 }' <<< $stage)	
-			case $colour in 
-				blue)
-					if [[ $operation -eq 2 ]]; then
-						if [[ $number -gt $blue ]]; then
-							blue=$number
-						fi
-					else
-						if [[ $number -gt 14 ]]; then
-							pass="false"
-						fi
-					fi
-				;;
-				green)
-					if [[ $operation -eq 2 ]]; then
-						if [[ $number -gt $green ]]; then
-							green=$number
-						fi
-					else
-						if [[ $number -gt 13 ]]; then
-							pass="false"
-						fi
-					fi
-				;;
-				red)
-					if [[ $operation -eq 2 ]]; then
-						if [[ $number -gt $red ]]; then
-							red=$number
-						fi
-					else
-						if [[ $number -gt 12 ]]; then
-							pass="false"
-						fi
-					fi
-				;;
-			esac
-		done
-	done
-	if [[ $operation -eq 2 ]]; then
+		b=${maximums["blue"]}
+		let blue+=$b
+		r=${maximums["red"]}
+		let red+=$r
+		g=${maximums["green"]}
+		let green+=$g
 		power=$((${blue}*${red}*${green}))
 		power_array+=("${power}")
 	else
